39b3767b19903731ca23b3fd8d3d00c587d3b48b
Ariel Rivera <rivera182@gmail.com>
2020-02-15 16:16:41 -0500

    app can get data from api

---
+++ scripts/script.js
@@ -0,0 +1,53 @@
+const warplaneRankings = {
+
+}
+
+warplaneRankings.apiKey = 'fb723aa882d3a2fa5be1190069572588';
+
+warplaneRankings.getPlaneStats = function (planeIds) {
+    $.ajax({
+        url: 'https://api.worldofwarplanes.com/wowp/encyclopedia/planeinfo/',
+        method: 'GET',
+        dataType: 'json',
+        data: {
+            application_id: this.apiKey,
+            plane_id: planeIds
+        }
+    }).then(function(response) {
+        console.log(response);
+    })
+}
+
+warplaneRankings.getPlaneData = function (nation, type) {
+    $.ajax({
+        url: 'https://api.worldofwarplanes.com/wowp/encyclopedia/planes/',
+        method: 'GET',
+        dataType: 'json',
+        data: {
+            application_id: this.apiKey,
+            nation: nation,
+            type: type
+        }
+    }).then(function(response) {
+        const planeIds = Object.keys(response.data);
+        warplaneRankings.getPlaneStats(planeIds.join(','));
+
+    })
+}
+
+
+warplaneRankings.init = function () {
+    $('form').on('submit', function (e) {
+        e.preventDefault();
+        const $userNation = $('input[name = nation]:checked').attr("id");
+        const $userType = $('input[name = type]:checked').attr("id");
+        warplaneRankings.getPlaneData($userNation, $userType);
+
+    })
+}
+
+$(function () {
+    warplaneRankings.init();
+})
+
+
